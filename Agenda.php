27b2e818a8b76871a65c4ea75d55a155560ea702
AdrianDW32 <agarciago20dw@ikzubirimanteo.com>
2021-10-08 12:30:43 +0200

    Creando el archivo Agenda.php

---
+++ Agenda.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda</title>
+</head>
+<body>
+    
+    <!-- FORMULARIO INGRESO DATOS -->
+
+    <?php
+        // CLASE AGENDA
+    ?>
+
+    <?php
+        // INSTANCIACIÓN Y LLAMADAS
+    ?>
+
+    <!-- TABLA MOSTRAR INFO -->
+
+</body>
+</html>
