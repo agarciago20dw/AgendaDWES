52b8c51d6193bc3811db55986dcfc711a799041f
AdrianDW32 <agarciago20dw@ikzubirimanteo.com>
2021-10-08 12:56:45 +0200

    Definiendo estructura (formulario, clase Agenda, tabla...) del archivo Agenda.php

--- Agenda.php
+++ Agenda.php
@@ -9,16 +9,73 @@
 <body>
     
     <!-- FORMULARIO INGRESO DATOS -->
+    <center>
+        <form method="post">
+            <table style="text-align: center;">
+                <tr>
+                    <td colspan="2"><h1>INTRODUCIR ENTRADA</h1></td>
+                </tr>
+                <tr>
+                    <td><label>Introduce el nombre</label></td>
+                    <td><input type="text" name="nombre"></td>
+                </tr>
+                <tr>
+                    <td><label>Introduce el correo electrónico</label></td>
+                    <td><input type="text" name="correo"></td>
+                </tr>
+                <tr>
+                    <td colspan="2"><input type="submit" value="ENVIAR" style="margin-top: 10px;"></td>
+                </tr>
+            </table> 
+            <!-- <input type="hidden" name="nombres" value="<?php  ?>"> -->
+        </form>
+    </center>
 
+    <!-- CLASE AGENDA -->
     <?php
-        // CLASE AGENDA
+        class Agenda {
+            // ATRIBUTO DE LA CLASE, ARRAY ASOCIATIVO (CLAVE: NOMBRE, VALOR: CORREO) DÓNDE GUARDAREMOS LAS ENTRADAS DE LA AGENDA
+            private $entradas;
+
+            // FUNCIÓN QUE AÑADE UNA ENTRADA A LA AGENDA - RECIBE UN NOMBRE Y UN CORREO POR PARÁMETROS
+            public function anadirEntrada($nombre, $correo) {
+                if (!$this->existeEntrada($nombre)) {
+                   $entradas[$nombre] = $correo; 
+                }
+                else {
+                    // MENSAJE ERROR
+                }
+            }
+
+            // FUNCIÓN PRIVADA QUE COMPRUEBA SI EXISTE ALGUNA ENTRADA EN EL ARRAY CON LA CLAVE RECIBIDA POR PARÁMETRO
+            private function existeEntrada($nombre) {
+                if (array_key_exists($nombre, $this->entradas)) {
+                    return true;
+                }
+                return false;
+            }
+        }
     ?>
 
+    <!-- INSTANCIACIÓN Y LLAMADAS -->
     <?php
-        // INSTANCIACIÓN Y LLAMADAS
+        $agenda = new Agenda();
     ?>
 
     <!-- TABLA MOSTRAR INFO -->
+        <br>
+        <center>
+            <table style="text-align: center;">
+                <tr>
+                    <td>NOMBRE</td>
+                    <td>CORREO</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </table>
+        </center>
 
 </body>
 </html>
